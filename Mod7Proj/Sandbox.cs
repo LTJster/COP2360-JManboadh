ac853ef36493991fcfc38f8235bacebc487d78a3
LTJster <ska_core@comcast.net>
2024-06-14 14:17:01 -0400

    Update Sandbox.cs
    Starting code for the MLB Dictionary setup

--- Mod7Proj/Sandbox.cs
+++ Mod7Proj/Sandbox.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static Dictionary<string, List<string>> mlbTeams = new Dictionary<string, List<string>>();
+
+    static void PopulateDictionary()
+    {
+        mlbTeams["Boston Red Sox"] = new List<string> { "Fenway Park", "David Ortiz", "Mookie Betts" };
+        mlbTeams["New York Yankees"] = new List<string> { "Yankee Stadium", "Derek Jeter", "Aaron Judge" };
+        mlbTeams["Houston Astros"] = new List<string> { "Minute Maid Park", "Jose Altuve", "George Springer" };
+    }
+    
+    static void Main(string[] args)
+    {
+        PopulateDictionary();
+        Console.WriteLine("Dictionary populated.");
+    }
